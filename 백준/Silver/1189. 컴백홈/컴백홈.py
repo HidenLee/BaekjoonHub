2e249932e00beb57e8eaee041993872cd2e92227
Hyundo Lee <hyundo1995@gmail.com>
2024-06-25 21:15:15 +0900

    [Silver I] Title: 컴백홈, Time: 236 ms, Memory: 113532 KB -BaekjoonHub

---
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -0,0 +1,25 @@
+#https://www.acmicpc.net/problem/1189
+
+delta = [(0,-1),(1,0),(0,1),(-1,0)]
+
+R, C, K = map(int, input().split())
+table = [list(input()) for _ in range(R)]
+visited = [[False for _ in range(C)] for _ in range(R)]
+def backtracking(x,y,depth,cnt):
+    visited[y][x] = True
+    
+    if depth > K: return cnt
+
+    if y == 0 and x == C - 1 and depth == K :
+        return cnt + 1
+    
+    for dt in delta:
+        nx = x + dt[0]
+        ny = y + dt[1]
+        if 0<= nx < C and 0 <= ny < R and not visited[ny][nx] and table[ny][nx] != "T":
+            visited[ny][nx] = True
+            cnt = backtracking(nx,ny,depth+1,cnt)
+            visited[ny][nx] = False
+    return cnt
+
+print(backtracking(0,R-1,1,0))
