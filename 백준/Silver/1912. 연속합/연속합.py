488cba15865ba2a6b131de40241e4c538ac8d234
Hyundo Lee <hyundo1995@gmail.com>
2024-08-26 09:31:23 +0900

    [Silver II] Title: 연속합, Time: 108 ms, Memory: 126836 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,13 +1,7 @@
 # https://www.acmicpc.net/problem/1912
 n = int(input())
 arr = list(map(int,input().split()))
-
-dp = [arr[0] for _ in range(n)]
-ans = arr[0]
+dp = [arr[0]]
 for idx in range(1,n):
-    dp[idx] = max(arr[idx],dp[idx-1]+arr[idx])
-    if ans < dp[idx]:
-        ans = dp[idx]
-# print(max(dp))
-print(ans)
-
+    dp.append(max(arr[idx],dp[idx-1]+arr[idx]))
+print(max(dp))
