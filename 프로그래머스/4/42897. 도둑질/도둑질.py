0e8a5bdf64aa1f1d6e14c094ba6599368e36d5eb
Hyundo Lee <hyundo1995@gmail.com>
2026-06-04 19:27:49 +0900

    [level 4] Title: 도둑질, Time: 571.62 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -5,6 +5,6 @@ def solution(money):
         dp[0][1] = arr[0]
         for i in range(1,len(arr)):
             dp[i][0] = max(dp[i-1][0], dp[i-1][1])
-            dp[i][1] = max(dp[i-1][1],dp[i-1][0] + arr[i])
+            dp[i][1] = dp[i-1][0] + arr[i]
         return max(dp[-1])
     return max(count(money[1:]),count(money[:-1]))
